7a07f13d0f9ba5f41d1ed6d2d1d25bc43388859e
Muhammad Adeel <91562562+madeel7258@users.noreply.github.com>
2023-08-22 15:37:58 +0500

    y

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -130,6 +130,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# settings.py
+import os
+
+# ...
+
+# Define the absolute path to your static files directory
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
