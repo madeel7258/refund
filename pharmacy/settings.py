c953ca3197582bd4cdfcf7d2579a2911a54ce4b8
Muhammad Adeel <91562562+madeel7258@users.noreply.github.com>
2023-08-22 15:38:42 +0500

    a

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -143,6 +143,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Add manually
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static")
-]
